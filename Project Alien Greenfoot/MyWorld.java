0facb5207608fc63f808e3643fa6a004d38c9a32
waradiana <113961968+waradiana@users.noreply.github.com>
2022-10-17 18:33:54 +0800

    Add files via upload

---
+++ Project Alien Greenfoot/MyWorld.java
@@ -0,0 +1,49 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+     
+    private void prepare()
+    {
+        Hero hero = new Hero();
+        addObject(hero,100,100);
+
+        hero.setLocation(78,194);
+    }
+    
+    public void act()
+    {
+        spawnEnemy();
+    }
+    
+    public int timerEnemy = 0;
+    public void spawnEnemy()
+    {
+        if (timerEnemy==180) //jika timer = 3 detik
+        {
+            //int speed = Greenfoot.getRandomNumber(4) + 1;
+            addObject(new Enemy(Greenfoot.getRandomNumber(4) + 1),599,Greenfoot.getRandomNumber(400));
+            timerEnemy = 0;
+        }else
+        {
+            timerEnemy++;
+        }
+    }
+}
